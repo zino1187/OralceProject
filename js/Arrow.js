54a43962948a73eaf7a79b7e62bdc8431d611293
koitt-04-P-PC\koitt-04-P <zino1187@gmail.com>
2018-10-27 10:49:03 +0900

    no message

---
+++ js/Arrow.js
@@ -0,0 +1,23 @@
+/*
+재사용가능성이 높은 코드는 클래스로 정의한다
+*/
+class Arrow{
+    constructor(){
+        this.span;
+        this.x=0;
+        this.span=document.createElement("span");
+        this.span.innerText="→";            
+        //css 적용 
+        this.span.style.position="absolute";
+        this.span.style.left=0+"px";
+        this.span.style.top=100+"px";
+        this.span.style.fontSize=30+"px";
+        document.body.appendChild(this.span);
+    }  
+
+    move(){
+        this.x+=10;
+        this.span.style.left=this.x+"px";
+    }       
+
+}
